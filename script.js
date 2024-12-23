d50f5986622e0ba5668af500f759c4632480df54
Pushpendra kumar sahu <153269194+Pushpendra2108@users.noreply.github.com>
2024-12-23 17:49:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	// Regular Expression to validate email format
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  
+  // Test the input string against the regex and return the result
+  return emailRegex.test(str);  
 }
 
 // Do not change the code below.
